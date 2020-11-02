870e924a3127e5a2caefffec3d8102bdc793f55d
Shay Ronen <Shayronen138@gmail.com>
2020-11-02 19:14:38 +0200

    message

--- server.js
+++ server.js
@@ -30,11 +30,7 @@ mongoose
 app.use("/api/customers", customers);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+  app.use(express.static(("client", "build")));
 }
 
 const PORT = process.env.PORT || 5000;
