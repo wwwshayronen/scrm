0ed4f794888346aaafd9dc03ae8cc48199c663a6
Shay Ronen <Shayronen138@gmail.com>
2020-11-02 19:01:12 +0200

    minor changes

--- server.js
+++ server.js
@@ -29,10 +29,11 @@ mongoose
 // use routes
 app.use("/api/customers", customers);
 
-if (process.env.NODE.ENV === "production") {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
