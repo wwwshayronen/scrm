393ed82b09b16377cdd19432fef5bc19a714546f
wwwshayronen <47533456+wwwshayronen@users.noreply.github.com>
2020-11-07 18:46:42 +0200

    Update HomePage.js
    upgrade a little bit the style

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -2,19 +2,31 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "antd";
 
-import OrganizeWoman from './OrganizeWoman'
+import OrganizeWoman from "./OrganizeWoman";
 
 const HomePage = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <div style={{display: "flex", justifyContent:"center", alignItems:"center", flexDirection:"column"}}>
-      <h1>SCRM - Simple crm</h1>
-      <p style={{fontSize:"1.1rem", fontWeight:"500"}}>Super easy-to-use CRM</p>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        margin: "0 auto",
+      }}
+    >
+      <h1 style={{ color: "#6c63ff", fontSize: "2rem", fontWeight: "600" }}>
+        SCRM - Simple crm
+      </h1>
+      <p style={{ color: "black", fontSize: "1.2rem", fontWeight: "500" }}>
+        Super easy-to-use CRM
+      </p>
       <Button
         style={{
-            width:"270px",
+          width: "270px",
           fontWeight: "bold",
-          fontSize:"0.9rem"
+          fontSize: "0.9rem",
         }}
         onClick={() => loginWithRedirect()}
       >
@@ -26,3 +38,4 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
