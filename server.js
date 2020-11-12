cfab9c80cdbdce920e68c2ef8d52b106f59ed40a
wwwshayronen <47533456+wwwshayronen@users.noreply.github.com>
2020-11-12 12:33:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use("/api/customers", customers);
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build'), function(err) {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
