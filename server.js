c1f83544f62b65c6dd837158d98e94eea6e85a4c
Shay Ronen <shayronen138@gmail.com>
2021-11-04 15:05:05 +0200

    add time interval to app listen function

--- server.js
+++ server.js
@@ -41,4 +41,4 @@ app.get("*", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, 3000, () => console.log(`server running on port ${PORT}`));
